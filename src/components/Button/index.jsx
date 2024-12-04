190e6d7fe0e6fcc04c53172e15406d9967175926
Rafael Trabalho <rafakroz@gmail.com>
2024-12-04 15:00:34 -0300

    Componente button finalizado

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,20 @@
+import './styles.css';
+
 import { Component } from "react";
 
 export class Button extends Component {
     render() {
         //Obtendo o valor da propriedade text do button do arquivo da home
-        const { text, onClick } = this.props;
+        const { text, onClick, disabled } = this.props;
 
         //Inserindo o valor da propridade no botão em si
         return (
-            <button 
+            <button
+                className='button'
                 onClick={onClick}
+                disabled={disabled}
             >
-                { text }
+                <span className='text-button'>{ text }</span>
             </button>
         );
     }
