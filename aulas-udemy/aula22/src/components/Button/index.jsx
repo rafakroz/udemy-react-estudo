1cc46b3b5a87966a7eb17776be768c7b95ceeb87
Rafael Trabalho <rafakroz@gmail.com>
2024-12-04 15:48:14 -0300

    Arquivos movidos para pasta aula-udemy

---
+++ aulas-udemy/aula22/src/components/Button/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+import { Component } from "react";
+
+export class Button extends Component {
+    render() {
+        //Obtendo o valor da propriedade text do button do arquivo da home
+        const { text, onClick, disabled } = this.props;
+
+        //Inserindo o valor da propridade no botão em si
+        return (
+            <button
+                className='button'
+                onClick={onClick}
+                disabled={disabled}
+            >
+                <span className='text-button'>{ text }</span>
+            </button>
+        );
+    }
+}
