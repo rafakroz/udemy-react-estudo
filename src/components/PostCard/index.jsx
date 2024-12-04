b76ee15c5c816659c4d4015462c1dc4f05c52822
Rafael Trabalho <rafakroz@gmail.com>
2024-12-04 15:28:41 -0300

    Estilo css movido para a pasta do PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,8 +1,10 @@
+import './styles.css';
+
 export const PostCard = ({title, cover, body, id}) => (
     <div className='post'>
         <img src={cover} alt={title} />
         <div className='post-content'>
-            <h2 >{title}</h2>
+            <h2 >{id} - {title}</h2>
             <p>{body}</p>
         </div>
     </div>
