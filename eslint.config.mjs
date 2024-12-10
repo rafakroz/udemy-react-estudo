ecdf7b889a12fb1ab05c782f2ebcf06195110429
Rafael Trabalho <rafakroz@gmail.com>
2024-12-10 16:59:11 -0300

    Arquivos de configurações alterados.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
