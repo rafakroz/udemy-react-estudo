b8eb0bea8fd6841f53872083a35858f69e19b671
Rafael Trabalho <rafakroz@gmail.com>
2024-12-10 16:57:40 -0300

    Efetuada a retificação do eslint

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import P from 'prop-types';
+
 import './styles.css';
 
 export const TextInput = ({ searchValue, handleChange }) => {
@@ -10,4 +14,9 @@ export const TextInput = ({ searchValue, handleChange }) => {
             placeholder='Type your search'
         />
     )
+}
+
+TextInput.propTypes = {
+  searchValue:  P.string.isRequired,
+  handleChange: P.func.isRequired,
 }
