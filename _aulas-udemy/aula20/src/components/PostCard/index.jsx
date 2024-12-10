0666f8056456aaa9c5edfd7ce8efd4900ea0f336
Rafael Trabalho <rafakroz@gmail.com>
2024-12-10 13:21:30 -0300

    Arquivo de test e pasta de snapshot gerada

--- _aulas-udemy/aula20/src/components/PostCard/index.jsx
+++
@@ -1,9 +0,0 @@
-export const PostCard = ({title, cover, body, id}) => (
-    <div className='post'>
-        <img src={cover} alt={title} />
-        <div className='post-content'>
-            <h1 >{title}</h1>
-            <p>{body}</p>
-        </div>
-    </div>
-);
--- _aulas-udemy/aula21/src/components/PostCard/index.jsx
+++
@@ -1,9 +0,0 @@
-export const PostCard = ({title, cover, body, id}) => (
-    <div className='post'>
-        <img src={cover} alt={title} />
-        <div className='post-content'>
-            <h2 >{title}</h2>
-            <p>{body}</p>
-        </div>
-    </div>
-);
