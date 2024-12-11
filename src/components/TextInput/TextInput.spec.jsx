7e7bef7a80d26a6f2bf936e91a9efa07e3402e3c
Rafael Trabalho <rafakroz@gmail.com>
2024-12-11 09:32:50 -0300

    Import React inserida

--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { render, screen } from "@testing-library/react";
 import { TextInput } from ".";
 import userEvent from "@testing-library/user-event";
@@ -6,7 +8,7 @@ describe('<TextInput />', () => {
 
     /* Verificar se há valor no searchValue */
     it('should have a value of searchValue', () => {
-        /* A função não precisa fazer nada. Ela é criada apenas para verificar se uma 
+        /* A função não precisa fazer nada. Ela é criada apenas para verificar se uma
         função específica foi chamada. */
         const fn = jest.fn();
 
@@ -16,13 +18,13 @@ describe('<TextInput />', () => {
 
         expect(input.value).toBe('testando');
     });
-    
+
     /* Verifica se a função é chamada a cada digito do teclado */
     it('should call handleChange function on each key pressed', () => {
         //
         const fn = jest.fn();
 
-        render(<TextInput handleChange={fn} />);
+        render(<TextInput handleChange={fn} searchValue="valor buscado" />);
 
         const input = screen.getByPlaceholderText(/type your search/i);
 
@@ -32,17 +34,17 @@ describe('<TextInput />', () => {
         userEvent.type(input, value);
 
         // Testando se o valor do input é igual ao value
-        expect(input.value).toBe(value);
+        expect(input.value).toBe('valor buscado');
 
         // Saber se a função foi chamada na quantidade de dígitos de const value
         expect(fn).toHaveBeenCalledTimes(value.length);
     });
-    
+
     it('sould match snapshot', () => {
         //
         const fn = jest.fn();
 
-        const { container } = render(<TextInput handleChange={fn} />);
+        const { container } = render(<TextInput handleChange={fn} searchValue="" />);
 
         expect(container).toMatchSnapshot();
     });
