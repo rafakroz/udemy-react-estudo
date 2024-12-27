a740a60bcac392d47018b9f93755d235660c0d77
Rafael Queiroz <rafakroz@gmail.com>
2024-12-27 13:23:16 -0300

    Hook criado

--- _aulas/s04_57_criando_hook/src/App.js
+++ _aulas/s04_57_criando_hook/src/App.js
@@ -1,93 +1,80 @@
-// import P from 'prop-types';
-import { createContext, useContext, useReducer, useRef } from 'react';
-import P from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-/* useReducer
- */
+/* Criando hook */
 
-// ----- actions.js -----
-export const actions = {
-  CHANGE_TITLE: 'CHANGE_TITLE',
-}
+  /* setInterval é uma função do próprio navegador, presente no node também.
+  Ela recebe (pode receber) uma função um um delay em milisegundos.
+  No exemplo, o delay default (caso não seja passado), foi definido como 1 segundo.  */
 
-// ----- data.js -----
-export const globalState = {
-  title:  'Aula: useContext e useReducer',
-  body:   'O body do Contexto',
-  counter: 0,
+const useMyHook = (callback, delay = 1000) => {
+  const savedCallback = useRef();
+
+  // Para lembrar qual foi o último callback chamado
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  /* Como o delay é uma dependência, toda vez que ele mudar, este useEffect será chamado */
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCallback.current();
+    }, delay);
+
+    // Toda vez que sair do componente o intervalo será limpado
+    return () => clearInterval(interval);
+  }, [delay]);
 };
 
-// ----- reducer.js -----
-export const reducer = (state, action) => {
-  switch(action.type) {
-    case actions.CHANGE_TITLE: {
-      console.log('Mudar título');
-      return {...state, title: action.payload};
-    }
-    default: {
-      return {...state};
-    }
-  }
-}
-
-// Componente de contexto
-export const Context = createContext();
-
-// ----- AppContext.jsx -----
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: actions.CHANGE_TITLE, payload })
-  }
-
-  return (
-    <Context.Provider value={{ state, changeTitle}}>
-      { children }
-    </Context.Provider>
-  );
-}
-
-// Componente com proptype
-// AppContext com children
-AppContext.propTypes = {
-  children: P.node,
-};
-
-// ----- H1 / index.jsx -----
-export const H1 = (props) => {
-  const context = useContext(Context);
-  // Referência do input, apenas para pegar o value
-  const inputRef = useRef();
-
-  const styles = {
-    inputInsert: {
-      marginLeft: '6px',
-      width: '120px'
-    }
-  }
-
-  return (
-    <>
-      <h1 onClick={() => context.changeTitle( inputRef.current.value )}>
-        { context.state.title }
-      </h1>
-
-      <input type='text' ref={inputRef} style={ styles.inputInsert } />
-    </>
-  );
-}
-
-// ----- App.jsx -----
 function App() {
-  //
+  /* Contador iniciado como 0 */
+  const [counter, setCounter] = useState(0);
+
+  /* Delay iniciado como 1 segundo */
+  const [delay, setDelay] = useState(1000);
+
+  /* */
+  const [incrementor, setIncrementor] = useState(200);
+
+  const buttonLeft = {
+    marginLeft: '10px',
+    cursor: 'pointer'
+  };
+
+  const buttonRight = {
+    marginLeft: '10px',
+    cursor: 'pointer'
+  };
+
+  /* Usando o hook criado
+  O hook, tem 2 parâmetros callback e delay */
+  useMyHook(
+    () => setCounter((c) => c + 1), delay
+  );
+
   return (
-    <AppContext>
-      <div>
-        <H1 />
-      </div>
-    </AppContext>
+    <div>
+      <h1>Criando hook</h1>
+      <h2>Contador: {counter}</h2>
+      <h2>Delay: {delay}</h2>
+
+      <input
+        type='number' value={incrementor}
+        onChange={(e) => setIncrementor(Number(e.target.value))}
+        ></input>
+
+      <button
+        style={{ ...buttonLeft }}
+        onClick={() => {setDelay(d => d + incrementor)}}
+        >+ {incrementor}
+      </button>
+
+      <button
+        style={{ ...buttonRight }}
+        onClick={() => {setDelay(d => d - incrementor)}}
+        >- {incrementor}
+      </button>
+    </div>
   );
 }
 
